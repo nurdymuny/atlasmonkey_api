20a42f2c1887d6a4d1b1abe1bef96a6577aa6500
Brian Russel Davis <brian@nurdymuny.com>
2015-05-11 15:56:51 +0530

    Changed mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_mailer.default_url_options = { host: '52.5.49.148' }
-  config.assets.raise_runtime_errors = false
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -77,4 +75,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.raise_runtime_errors = false
+  Rails.application.routes.default_url_options[:host] = '52.5.49.148'
+  ActionMailer::Base.smtp_settings = {
+
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :domain => 'gmail.com',
+  :user_name => 'test@gmail.com',
+  :password => 'test123',
+  :authentication => 'login',
+  :enable_starttls_auto => true
+  }
 end
