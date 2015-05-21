5ec5c91f3a40068f200417388076787fac2d222b
Brian Russel Davis <brian@nurdymuny.com>
2015-05-21 12:19:51 +0530

    Minor changes for api for error messages

--- app/controllers/api/v1/blocks_controller.rb
+++ app/controllers/api/v1/blocks_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BlocksController < ApplicationController
   load_and_authorize_resource :except => [:index]
   def index
-    @venue = Venue.find(params[:venue_id])
+    @venue = Venue.find_by_id(params[:venue_id])
     @blocks = @venue.blocks
     render json: {:success => true, :data => {venue: @venue.as_json(include: :blocks) } }
   end
--- app/controllers/api/v1/seats_controller.rb
+++ app/controllers/api/v1/seats_controller.rb
@@ -1,9 +1,17 @@
 class Api::V1::SeatsController < ApplicationController
   
   def index
-    @venue = Venue.find(params[:venue_id])
-    @block = @venue.blocks.find(params[:block_id])
-    render json: {:success => true, :data => {block: @block.as_json(except: [:created_at, :updated_at], include:  { seats: { except: [:created_at, :updated_at] } } ) } }
+    @venue = Venue.find_by_id(params[:venue_id])
+    if @venue.present?
+      @block = @venue.blocks.find_by_id(params[:block_id])
+      if @block.present?
+        render json: {:success => true, :data => {block: @block.as_json(except: [:created_at, :updated_at], include:  { seats: { except: [:created_at, :updated_at] } } ) } }
+      else
+        render json: { :success => false, :errors => "No block exist for the provided block id"}
+      end
+    else
+      render json: { :success => false, :errors => "No venue exist for the provided venue id"}
+    end
   end
   
 end
