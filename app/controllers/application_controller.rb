54035b11cbe95d5ecb32f1081affec7447b640a5
Brian Russel Davis <brian@nurdymuny.com>
2015-09-23 15:31:30 +0530

    Authenticated user before access any page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => "atlasmonkey", :password => "am@staging", if: :html_request?
+  before_action :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #load_and_authorize_resource
--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,7 +1,9 @@
 class BlocksController < ApplicationController
+  before_action :find_venue
+  before_action :find_block, except: [:new, :create, :index]
   load_and_authorize_resource
+  
   def index
-    @venue = Venue.find(params[:venue_id])
     @blocks = @venue.blocks
     respond_to do |format|
       format.json { render json: {:success => true, :data => {venue: @venue.as_json(include: :blocks) } }}
@@ -10,12 +12,10 @@ class BlocksController < ApplicationController
   end
   
   def new
-    @venue = Venue.find(params[:venue_id])
     @block = @venue.blocks.new
   end
   
   def create
-    @venue = Venue.find(params[:venue_id])
     @block = @venue.blocks.new(block_params)
     respond_to do |format|
       if @block.save
@@ -28,14 +28,13 @@ class BlocksController < ApplicationController
     end
   end
   
+  def show
+  end
+  
   def edit
-    @venue = Venue.find(params[:venue_id])
-    @block = @venue.blocks.find(params[:id])
   end
   
   def update
-    @venue = Venue.find(params[:venue_id])
-    @block = @venue.blocks.find(params[:id])
     respond_to do |format|
       if @block.update_attributes(block_params)
         format.json {render json: {:success => true, :data => {message: "Block updated successfully." } }}
@@ -48,8 +47,6 @@ class BlocksController < ApplicationController
   end
   
   def destroy
-    @venue = Venue.find(params[:venue_id])
-    @block = @venue.blocks.find(params[:id])
     @block.destroy
     respond_to do |format|
       format.json {render json: {:success => true , :data => {message: "Block deleted successfully." } }}
@@ -58,7 +55,18 @@ class BlocksController < ApplicationController
   end
   
   private
-    def block_params
-      params.require(:block).permit(:name, :no_of_seats, :left, :right, :top_left, :top_right)
-    end
+    
+  def block_params
+    params.require(:block).permit(:name, :no_of_seats, :left, :right, :top_left, :top_right)
+  end
+  
+  def find_venue
+    @venue = Venue.find(params[:venue_id])
+  end
+    
+  def find_block
+    find_venue
+    @block = @venue.blocks.find(params[:id])
+  end  
+    
 end
