8fa20127f494ad5dd3fd810b028076625d96050f
Brian Russel Davis <brian@nurdymuny.com>
2015-10-05 13:36:38 +0530

    fix authentication bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => "atlasmonkey", :password => "am@staging", if: :html_request?
-  before_action :authenticate_user!
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #load_and_authorize_resource
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    if current_user.is_admin
+    if current_user and current_user.is_admin
       redirect_to venues_path, :alert => "successfuly sign_in"
     else
       redirect_to home_index_path, :alert => exception.message
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
     if current_user.present?
       @current_user = current_user
     else
-      @current_user = UserToken.find_by_auth_token(request.headers['auth-token']).user
+      @current_user = UserToken.find_by_auth_token(request.headers['auth-token']).try(:user)
     end
     @current_ability ||= Ability.new(@current_user)
   end
