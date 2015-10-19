06ac70deabb06087455391fd346d05e579564797
Brian Russel Davis <brian@nurdymuny.com>
2015-10-19 16:16:40 +0530

    Validate user for venue's seat

--- app/models/seat.rb
+++ app/models/seat.rb
@@ -6,4 +6,17 @@ class Seat < ActiveRecord::Base
   has_one :ticket, dependent: :destroy
   
   validates_presence_of :seat_number, :latitude, :longitude
+  
+  before_save :uniqueness_of_user_for_venue, if: ->(obj){ obj.user_id.present? }
+  
+  private
+  
+  def uniqueness_of_user_for_venue
+    level = block.level
+    user_seat = Seat.joins(:block => :level).where("levels.venue_id =? && seats.user_id =?", level.venue_id, self.user_id)
+    if user_seat.count >= 1
+      self.errors.add(:base, "Seat is already assign to this user for #{level.venue.name} venue")
+      return false
+    end
+  end
 end
